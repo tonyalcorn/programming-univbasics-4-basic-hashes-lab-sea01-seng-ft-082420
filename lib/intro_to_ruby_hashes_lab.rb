9970c859a64cae154d0da897ad40beed9dee580e
Anthony Alcorn <xmastony@gmail.com>
2020-08-15 23:33:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,10 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  new_hash = {
+    :wrestler => "Goldberg",
+    :boxer => "Mike Tyson"
+  }
 end
 
 def pioneer
